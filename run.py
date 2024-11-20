6c3f5a37a08a0cc9d5310252c44b4dc78d0719c2
Tianli <507249007@qq.com>
2024-11-20 19:53:53 +0800

    ✨ feat: 更新YOLO模型加载方式，添加图像目录批量识别功能并调整窗口大小

--- run.py
+++ run.py
@@ -1,12 +1,29 @@
+import cv2
+import os
 from ultralytics import YOLO
 
 # 加载模型
-model = YOLO("../runs/detect/train7/weights/best.pt")
+model = YOLO("../runs/detect/train3/weights/best.pt")
 
-# 识别图片
-results = model(
-    source=0,
-    device="0",
-    show=True,
-    conf=0.5, # 针对car这个模型，0.6是最佳阈值
-)
+# 设置窗口大小
+window_name = "YOLO Detection"
+cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+cv2.resizeWindow(window_name, 1080, 1080)
+
+image_dir = "D:/develop/YoloDB/tt100k_2021/test"
+for image_name in os.listdir(image_dir):
+    image_path = os.path.join(image_dir, image_name)
+    image = cv2.imread(image_path)
+    
+    results = model(image)
+    
+    for result in results:
+        annotated_image = result.plot()
+        cv2.imshow(window_name, annotated_image)
+        
+        key = cv2.waitKey(0)
+        
+        if key == 27:  # 按下ESC键退出
+            break
+
+cv2.destroyAllWindows()
--- train.py
+++ train.py
@@ -6,4 +6,4 @@ model = YOLO("yolo11n.pt")  # 加载预训练模型（推荐用于训练）
 model = YOLO("yolo11n.yaml").load("yolo11n.pt")  # 从 YAML 文件构建模型并加载权重
 
 # 训练数据集
-results = model.train(data="D:/develop/YoloDB/tt100k_yolo/tt100k.yaml", epochs=5, workers=0,device=0)
+results = model.train(data="D:/develop/YoloDB/tt100k_yolo/tt100k.yaml", epochs=5, workers=0,device=0,imgsz=640)
