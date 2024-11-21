87df8fb35a9d8d8f93ceb2a3e3ca179a29c908fd
Tianli <507249007@qq.com>
2024-11-21 19:26:20 +0800

    ✨ feat: 更新模型路径并调整训练参数，优化训练设置

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 from ultralytics import YOLO
 
 # 加载模型
-model = YOLO("../runs/detect/train3/weights/best.pt")
+model = YOLO("../runs/detect/train8/weights/best.pt")
 
 # 设置窗口大小
 window_name = "YOLO Detection"
@@ -14,15 +14,15 @@ image_dir = "D:/develop/YoloDB/tt100k_2021/test"
 for image_name in os.listdir(image_dir):
     image_path = os.path.join(image_dir, image_name)
     image = cv2.imread(image_path)
-    
+
     results = model(image)
-    
+
     for result in results:
         annotated_image = result.plot()
         cv2.imshow(window_name, annotated_image)
-        
+
         key = cv2.waitKey(0)
-        
+
         if key == 27:  # 按下ESC键退出
             break
 
