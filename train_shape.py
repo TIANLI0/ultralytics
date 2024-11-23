5fe3ac7089f6fc62247f763f0793f9bcb5efafc8
Tianli <507249007@qq.com>
2024-11-23 18:14:08 +0800

    ✨ feat: 添加训练结束回调以同步训练详情到服务器，更新模型和数据集配置

--- train_shape.py
+++ train_shape.py
@@ -1,16 +1,55 @@
 from ultralytics import YOLO
+import json
+from datetime import datetime
+import sys
+import os
 
-# 读取模型
-model = YOLO("yolo11n")  # 加载预训练模型（推荐用于训练）
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from TrainSync import sync_to_server
 
-# 训练数据集
+
+def on_train_epoch_end(trainer):
+    """获取当前的训练详情并同步到服务器"""
+    data = {}
+    for key in dir(trainer):
+        value = getattr(trainer, key)
+        try:
+            json.dumps(value)
+            data[key] = value
+        except (TypeError, OverflowError):
+            continue
+
+    current_epoch = trainer.epoch + 1
+
+    data.update(
+        {
+            "model": modelA,
+            "train_device": str(deviceA),
+            "dataset": dataset,
+            "description": description,
+            "version": version,
+        }
+    )
+
+    sync_to_server(data, current_epoch)
+
+
+modelA = "../runs/detect/train10/weights/best.pt"
+description = "分组训练实验2"
+version = "Group 0.2"
+dataset = "D:/develop/YoloDB/tt100k_yolo_Shape/tt100k.yaml"
+deviceA = 0
+
+model = YOLO(modelA)
+
+model.add_callback("on_train_epoch_end", on_train_epoch_end)
+
 results = model.train(
-    data="D:/develop/YoloDB/tt100k_yolo_Shape/tt100k.yaml",
-    epochs=60,  # 训练轮数
-    workers=0,  # 数据加载器工作线程数，windows下设置为0
-    device=0,  # GPU id
+    data=dataset,
+    epochs=60,
+    workers=0,
+    device=deviceA,
     imgsz=640,
-    # time=3, # 限制训练时间（小时）
-    cache=True,  # 缓存数据集
-    plots=True,  # 绘制结果
+    cache=True,
+    plots=True,
 )
