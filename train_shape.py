056568e84da2c01bf0e2c76582ea8a1e1afcfbe1
Tianli <507249007@qq.com>
2024-11-24 18:31:33 +0800

    ✨ feat: 添加模型保存时更新arg.yaml文件并上传功能

--- train_shape.py
+++ train_shape.py
@@ -3,9 +3,11 @@ import json
 from datetime import datetime
 import sys
 import os
+import yaml
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from TrainSync import sync_to_server
+from TrainUpload import upload_files
 
 
 def on_train_epoch_end(trainer):
@@ -34,15 +36,53 @@ def on_train_epoch_end(trainer):
     sync_to_server(data, current_epoch)
 
 
-modelA = "../runs/detect/train10/weights/last.pt"
-description = "分组训练实验3"
-version = "Group 0.2-fix1"
-dataset = "D:/develop/YoloDB/tt100k_yolo_Shape/tt100k.yaml"
+def on_model_save(trainer):
+    """在模型保存时更新arg.yaml文件并上传文件"""
+    # 查找最新的训练文件夹
+    base_path = "../runs/detect"
+    if not os.path.exists(base_path):
+        os.makedirs(base_path)
+
+    latest_folder = max(
+        [
+            os.path.join(base_path, d)
+            for d in os.listdir(base_path)
+            if os.path.isdir(os.path.join(base_path, d))
+        ],
+        key=os.path.getmtime,
+    )
+
+    # 更新arg.yaml文件
+    arg_file = os.path.join(latest_folder, "args.yaml")
+    with open(arg_file, "r") as f:
+        args = yaml.safe_load(f)
+
+    args.update(
+        {
+            "version": version,
+            "train_device": "3060 laptop 6GB",
+            "dataset": "TT100K Tianli",
+            "description": description,
+        }
+    )
+
+    with open(arg_file, "w") as f:
+        yaml.safe_dump(args, f)
+
+    # 上传文件
+    upload_files(latest_folder, os.path.basename(latest_folder))
+
+
+modelA = "../runs/detect/train14/weights/last.pt"
+description = "分组分类实验1"
+version = "Classification 0.1"
+dataset = "D:/develop/YoloDB/tt100k_yolo/tt100k.yaml"
 deviceA = 0
 
 model = YOLO(modelA)
 
 model.add_callback("on_train_epoch_end", on_train_epoch_end)
+model.add_callback("on_train_end", on_model_save)
 
 results = model.train(
     data=dataset,
